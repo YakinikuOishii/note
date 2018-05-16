9510508fc735e8e2dd06faf544c1a8c29fb71d12
笠原未来 <fum_o3o7@ezweb.ne.jp>
2018-05-16 19:02:56 +0900

    手帳アプリ

---
+++ note/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  note
+//
+//  Created by 笠原未来 on 2018/05/15.
+//  Copyright © 2018年 笠原未来. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
--- note/ViewController.swift
+++ note/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class ViewController: UIViewController {
+    let formatter = DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // ナビゲーションバー
+        self.navigationController?.isToolbarHidden = false
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(named: "bg"), for: .topAttached, barMetrics: .default)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +24,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
+extension ViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2018 01 01")
+        let endDate = formatter.date(from: "2018 12 31")
+        
+        let parameters =  ConfigurationParameters(startDate: startDate!, endDate: endDate!)
+        return parameters
+    }
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.dateLabel.text  = cellState.text
+        return cell
+    }
+    
+}
